ef48da005bf768afeec5a1b4325f84470067474f
muhamadmansurov <muhammadmibnmaruf1@gmail.com>
2021-10-19 18:28:06 +0500

    new-post page

---
+++ src/screens/analitka/Analitka.syles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "../../constants/colors";
+
+export const AnalitkaContainer = styled.div `
+    /* margin-top: 45px;
+     */
+    /* padding: 40px; */
+`

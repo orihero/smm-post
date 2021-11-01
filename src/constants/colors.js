d435a1020154ce8d732d53ee3695de3647475c90
muhamadmansurov <muhammadmibnmaruf1@gmail.com>
2021-11-01 15:26:11 +0500

    news-push

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,11 +1,14 @@
 export let COLORS = {
     primaryPink: "#E8427E",
     white: "#fff",
-    darkBlue: '#102046',
     borderGray: "rgba(16, 32, 70, 0.15)",
     backgroundColor: "#F7FAFC",
     fontColors: "#102046",
     inputBorderColor: 'rgba(16, 32, 70, 0.1)',
     loginColors: '#006ED4',
+    googleBackgroundColor: '#F44336',
+    facebookBackgroundColor: '#3B5998',
+    vkontakteBackgroundColor: '#44678D',
+    twitterBackgroundColor: '#03A9F4',
 }
 
